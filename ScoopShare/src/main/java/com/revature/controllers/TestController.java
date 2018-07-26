ae795a551271dbc3efb87bb31cf6a372d723d03b
Karen Matney <matneyka@gmail.com>
2018-07-25 20:04:40 -0400

    Adding User, Articles, and some User Functionality for testing
    Testing Back-end and DBConnection through CodePipeline

---
+++ ScoopShare/src/main/java/com/revature/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.revature.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value="/test")
+public class TestController {
+	
+	@ResponseBody
+	@RequestMapping(method=RequestMethod.GET, produces=MediaType.TEXT_PLAIN_VALUE)
+	public String test() {
+		return "ScoopShare Test";
+	}
+}
---
+++ ScoopShare/src/main/java/com/revature/services/UserService.java
@@ -0,0 +1,36 @@
+package com.revature.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.beans.User;
+import com.revature.repositories.UserRepository;
+
+@Service
+@Transactional
+public class UserService {
+	static {
+		System.out.println("[DEBUG] - UserService instantiated...");
+	}
+	
+	@Autowired
+	UserRepository userRepo;
+	
+	public List<User> getAll() {
+		System.out.println("[DEBUG] - In UserService.getAll()...");
+		return userRepo.getAll();
+	}
+	
+	public User getById(int id) {
+		System.out.println("[DEBUG] - In UserService.getById()...");
+		return userRepo.getById(id);
+	}
+	
+	public User addUser(User u) {
+		System.out.println("[DEBUG] - In UserService.addUser()...");
+		return userRepo.addUser(u);
+	}
+}
